4706640b333cea2ab12b2786dd3855516f29165f
binarypsilocybin <rogeriosvaldo@gmail.com>
2021-12-30 22:04:34 +0000

    Updated counter

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,12 +1,44 @@
 import React, { Component } from 'react';
+import Button from 'react-bootstrap/Button';
 
 class Counter extends React.Component {
-  render() { 
-    return(<div>
-      <h1>Hello World</h1>
-      <button>Increment</button>;
+  state = {
+    count: this.props.value,
+    tags: []
+  };
+
+
+  handleIncrement = product => {
+
+    this.setState({ count: this.state.count + 1 })
+  };
+  renderTags(){
+    if(this.state.tags.length === 0) return <p>There are no tags!</p>;
+    return <ul>{ this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
+  }
+  render() {      
+    console.log(this.props);
+
+    return(
+    <div>
+      <h4>{this.props.id}</h4>
+      <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+      <button onClick={(product) => this.handleIncrement(product)} className="btn btn-secondary btn-sm">Increment</button>
+      <button onClick={this.props.onDelete} className="btn btn-danger btn-sm m-2">Delete</button>
+      {/* {this.state.tags.length === 0 && "Please create a new tag!"}
+      {this.renderTags()} */}
     </div>
     );
+  }
+  getBadgeClasses() {
+    let classes = "badge m-2 bg-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount(){
+    const { count } = this.state;
+    return count ===  0 ? 'Zero' : count;
   }
 }
  
--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.css'
-import Counter from './components/counter'
+import 'bootstrap/dist/css/bootstrap.css';
+import Counters from './components/counters';
 
-ReactDOM.render(<Counter/>,document.getElementById('root')
+ReactDOM.render(<Counters/>,document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
